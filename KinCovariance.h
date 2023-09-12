cf62ea81b94d005e82be963a431bf04005d4b91b
Pierre Chatagnon <pierrec@jlab.org>
2023-09-12 14:30:22 -0400

    Store errors in KinCovariance and allow to retrive it

--- KinCovariance.h
+++ KinCovariance.h
@@ -15,21 +15,28 @@ public:
     KinCovariance(TString in_File_path)
     {
         TFile *in_File = new TFile(in_File_path);
-        /*C_P_hist = *(TH3D*)in_File->Get("C_P_hist");
-        C_theta_hist = *(TH3D*)in_File->Get("C_theta_hist");
-        C_phi_hist = *(TH3D*)in_File->Get("C_phi_hist");
-        C_P_theta_hist = *(TH3D*)in_File->Get("C_P_theta_hist");
-        C_P_phi_hist = *(TH3D*)in_File->Get("C_P_phi_hist");
-        C_theta_phi_hist = *(TH3D*)in_File->Get("C_theta_phi_hist");*/
+        _C_P_hist = *(TH3D *)in_File->Get("C_P_hist");
+        _C_theta_hist = *(TH3D *)in_File->Get("C_theta_hist");
+        _C_phi_hist = *(TH3D *)in_File->Get("C_phi_hist");
+        _C_P_theta_hist = *(TH3D *)in_File->Get("C_P_theta_hist");
+        _C_P_phi_hist = *(TH3D *)in_File->Get("C_P_phi_hist");
+        _C_theta_phi_hist = *(TH3D *)in_File->Get("C_theta_phi_hist");
 
-        //The covariance matrix histogramms have various name, we must settle on one and stick to it
+        // The covariance matrix histogramms have various name, we must settle on one and stick to it
 
-        C_P_hist = *(TH3D *)in_File->Get("C_P_cut");
-        C_theta_hist = *(TH3D *)in_File->Get("C_theta_cut");
-        C_phi_hist = *(TH3D *)in_File->Get("C_phi_cut");
-        C_P_theta_hist = *(TH3D *)in_File->Get("C_P_theta_cut");
-        C_P_phi_hist = *(TH3D *)in_File->Get("C_P_phi_cut");
-        C_theta_phi_hist = *(TH3D *)in_File->Get("C_theta_phi_cut");
+        /*_C_P_hist = *(TH3D *)in_File->Get("C_P_cut");
+        _C_theta_hist = *(TH3D *)in_File->Get("C_theta_cut");
+        _C_phi_hist = *(TH3D *)in_File->Get("C_phi_cut");
+        _C_P_theta_hist = *(TH3D *)in_File->Get("C_P_theta_cut");
+        _C_P_phi_hist = *(TH3D *)in_File->Get("C_P_phi_cut");
+        _C_theta_phi_hist = *(TH3D *)in_File->Get("C_theta_phi_cut");*/
+
+        _C_P_err_hist = *(TH3D *)in_File->Get("C_P_err_hist");
+        _C_theta_err_hist = *(TH3D *)in_File->Get("C_theta_err_hist");
+        _C_phi_err_hist = *(TH3D *)in_File->Get("C_phi_err_hist");
+        _C_P_theta_err_hist = *(TH3D *)in_File->Get("C_P_theta_err_hist");
+        _C_P_phi_err_hist = *(TH3D *)in_File->Get("C_P_phi_err_hist");
+        _C_theta_phi_err_hist = *(TH3D *)in_File->Get("C_theta_phi_err_hist");
     }
 
     // Interpolate the covariance matrix to the momenta (in GeV) and angles (in degrees) provided as arguments
@@ -42,28 +49,27 @@ public:
 
         // So far studies are done for sector 2, requiering to do some arythmetics on phi using the sector number
         double Temp_Phi_in_vector = (phi < 0. && sector > 1) ? phi + 360 : phi;
-        double Phi_in_vector = (sector == 1) ? Temp_Phi_in_vector + 60 : Temp_Phi_in_vector - (sector - 2) * 60.; 
-
+        double Phi_in_vector = (sector == 1) ? Temp_Phi_in_vector + 60 : Temp_Phi_in_vector - (sector - 2) * 60.;
 
         // Diagonal terms
         // Momentum resolution
-        Cov_Matrix[0][0] = C_P_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[0][0] = _C_P_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
         // Theta resolution
-        Cov_Matrix[1][1] = C_theta_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[1][1] = _C_theta_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
         // Phi resolution
-        Cov_Matrix[2][2] = C_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[2][2] = _C_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
 
         // Off Diagonal terms
         // Momentum/Theta covariance
-        Cov_Matrix[0][1] = C_P_theta_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[0][1] = _C_P_theta_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
         Cov_Matrix[1][0] = Cov_Matrix[0][1];
 
         // Momentum/Phi covariance
-        Cov_Matrix[0][2] = C_P_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[0][2] = _C_P_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
         Cov_Matrix[2][0] = Cov_Matrix[0][2];
 
         // Phi/Theta covariance
-        Cov_Matrix[1][2] = C_theta_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Matrix[1][2] = _C_theta_phi_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
         Cov_Matrix[2][1] = Cov_Matrix[1][2];
 
         return Cov_Matrix;
@@ -74,7 +80,48 @@ public:
         return Interpolate(sector, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), in_vector.Phi() * TMath::RadToDeg());
     }
 
-    //Verify that the 8 bins around the point to interpolate are none zero, this is done for each 6 component of the cov. matrix
+    // Methods to get the error of the covariance matrix
+    TMatrixD Interpolate(int sector, double p, double theta, double phi)
+    {
+        TMatrixD Cov_Err_Matrix(3, 3);
+
+        double P_in_vector = p;
+        double Theta_in_vector = theta;
+
+        // So far studies are done for sector 2, requiering to do some arythmetics on phi using the sector number
+        double Temp_Phi_in_vector = (phi < 0. && sector > 1) ? phi + 360 : phi;
+        double Phi_in_vector = (sector == 1) ? Temp_Phi_in_vector + 60 : Temp_Phi_in_vector - (sector - 2) * 60.;
+
+        // Diagonal terms
+        // Momentum resolution
+        Cov_Err_Matrix[0][0] = _C_P_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        // Theta resolution
+        Cov_Err_Matrix[1][1] = _C_theta_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        // Phi resolution
+        Cov_Err_Matrix[2][2] = _C_phi_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+
+        // Off Diagonal terms
+        // Momentum/Theta covariance
+        Cov_Err_Matrix[0][1] = _C_P_theta_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Err_Matrix[1][0] = Cov_Err_Matrix[0][1];
+
+        // Momentum/Phi covariance
+        Cov_Err_Matrix[0][2] = _C_P_phi_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Err_Matrix[2][0] = Cov_Err_Matrix[0][2];
+
+        // Phi/Theta covariance
+        Cov_Err_Matrix[1][2] = _C_theta_phi_err_hist.Interpolate(P_in_vector, Theta_in_vector, Phi_in_vector);
+        Cov_Err_Matrix[2][1] = Cov_Err_Matrix[1][2];
+
+        return Cov_Err_Matrix;
+    }
+
+    TMatrixD Interpolate_Error(int sector, TLorentzVector in_vector)
+    {
+        return Interpolate_Error(sector, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), in_vector.Phi() * TMath::RadToDeg());
+    }
+
+    // Verify that the 8 bins around the point to interpolate are none zero, this is done for each 6 component of the cov. matrix
     bool Is_good_to_interpolate(int sector, TLorentzVector in_vector)
     {
         // very bad implementation
@@ -82,15 +129,15 @@ public:
         double Temp_Phi_in_vector = (phi < 0. && sector > 1) ? phi + 360 : phi;
         double Phi_in_vector = (sector == 1) ? Temp_Phi_in_vector + 60 : Temp_Phi_in_vector - (sector - 2) * 60.; // fmod(Temp_Phi_in_vector,60.)+60.;
 
-        return (Is_good_to_interpolate_hist(C_P_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
-                Is_good_to_interpolate_hist(C_theta_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
-                Is_good_to_interpolate_hist(C_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
-                Is_good_to_interpolate_hist(C_P_theta_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
-                Is_good_to_interpolate_hist(C_P_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
-                Is_good_to_interpolate_hist(C_theta_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector));
+        return (Is_good_to_interpolate_hist(_C_P_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
+                Is_good_to_interpolate_hist(_C_theta_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
+                Is_good_to_interpolate_hist(_C_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
+                Is_good_to_interpolate_hist(_C_P_theta_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
+                Is_good_to_interpolate_hist(_C_P_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector) &&
+                Is_good_to_interpolate_hist(_C_theta_phi_hist, in_vector.P(), in_vector.Theta() * TMath::RadToDeg(), Phi_in_vector));
     }
 
-    //Verify that the 8 bins around the point to interpolate are none zero, this is done for one specific component of the cov. matrix
+    // Verify that the 8 bins around the point to interpolate are none zero, this is done for one specific component of the cov. matrix
     bool Is_good_to_interpolate_hist(TH3D hist, double p, double theta, double phi)
     {
         // Ask that interpolation can be done only if each neighboor point have none zero values
@@ -120,12 +167,19 @@ public:
     }
 
 private:
-    TH3D C_P_hist;
-    TH3D C_theta_hist;
-    TH3D C_phi_hist;
-    TH3D C_P_theta_hist;
-    TH3D C_P_phi_hist;
-    TH3D C_theta_phi_hist;
+    TH3D _C_P_hist;
+    TH3D _C_theta_hist;
+    TH3D _C_phi_hist;
+    TH3D _C_P_theta_hist;
+    TH3D _C_P_phi_hist;
+    TH3D _C_theta_phi_hist;
+
+    TH3D _C_P_err_hist;
+    TH3D _C_theta_err_hist;
+    TH3D _C_phi_err_hist;
+    TH3D _C_P_theta_err_hist;
+    TH3D _C_P_phi_err_hist;
+    TH3D _C_theta_phi_err_hist;
 };
 
 #endif
